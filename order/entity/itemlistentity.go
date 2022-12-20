2251acfea5dff10d6c64d976c8a283ff815330ba
WJPone <474790700@qq.com>
2022-12-20 09:14:56 +0800

    feat:(order):增加订单明细图片对象

--- order/entity/itemlistentity.go
+++ order/entity/itemlistentity.go
@@ -6,28 +6,38 @@ import (
 
 //ItemListEntity
 type ItemListEntity struct {
-	ItemID                 int64   `json:"item_id"`
-	OrderItemID            int64   `json:"order_item_id"`
-	ItemName               string  `json:"item_name"`
-	ItemSku                string  `json:"item_sku"`
-	ModelID                int64   `json:"model_id"`
-	ModelName              string  `json:"model_name"`
-	ModelSku               string  `json:"model_sku"`
-	ModelQuantityPurchased int     `json:"model_quantity_purchased"`
-	ModelOriginalPrice     float32 `json:"model_original_price"`
-	ModelDiscountedPrice   float32 `json:"model_discounted_price"`
-	Wholesale              bool    `json:"wholesale"`
-	Weight                 float32 `json:"weight"`
-	AddOnDeal              bool    `json:"add_on_deal"`
-	MainItem               bool    `json:"main_item"`
-	AddOnDealID            int64   `json:"add_on_deal_id"`
-	PromotionType          string  `json:"promotion_type"`
-	PromotionID            int64   `json:"promotion_id"`
-	PromotionGroupID       int64   `json:"promotion_group_id"`
+	ItemID                 int64           `json:"item_id"`
+	OrderItemID            int64           `json:"order_item_id"`
+	ItemName               string          `json:"item_name"`
+	ItemSku                string          `json:"item_sku"`
+	ModelID                int64           `json:"model_id"`
+	ModelName              string          `json:"model_name"`
+	ModelSku               string          `json:"model_sku"`
+	ModelQuantityPurchased int             `json:"model_quantity_purchased"`
+	ModelOriginalPrice     float32         `json:"model_original_price"`
+	ModelDiscountedPrice   float32         `json:"model_discounted_price"`
+	Wholesale              bool            `json:"wholesale"`
+	Weight                 float32         `json:"weight"`
+	AddOnDeal              bool            `json:"add_on_deal"`
+	MainItem               bool            `json:"main_item"`
+	AddOnDealID            int64           `json:"add_on_deal_id"`
+	PromotionType          string          `json:"promotion_type"`
+	PromotionID            int64           `json:"promotion_id"`
+	PromotionGroupID       int64           `json:"promotion_group_id"`
+	ImageInfo              ImageInfoEntity `json:"image_info"`
 }
 
 //String
 func (i ItemListEntity) String() string {
+	return lib.ObjectToString(i)
+}
+
+type ImageInfoEntity struct {
+	ImageUrl string `json:"image_url"`
+}
+
+//String
+func (i ImageInfoEntity) String() string {
 	return lib.ObjectToString(i)
 }
 
